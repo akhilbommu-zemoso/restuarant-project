e36dd0069a555d2e5f37c8d117b75ea10aa3fe42
akhilbommu-zemoso <akhil.bommu@zemosolabs.com>
2021-08-18 11:53:11 +0530

    Restauarant Project 101

---
+++ src/main/java/com/akhil/springproject/dao/RestaurantRepository.java
@@ -0,0 +1,13 @@
+package com.akhil.springproject.dao;
+
+import com.akhil.springproject.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+}
